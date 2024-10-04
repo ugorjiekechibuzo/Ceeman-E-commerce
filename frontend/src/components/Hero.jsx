969abb0e70f8b142982feaa1ab4de1804a31586e
ugorji eke <ugorjiekechibuzo@gmail.com>
2024-10-04 17:08:42 +0100

    updated Hero's page

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className='flex flex-col sm:flex-row border border-gray-400'>
         {/* Hero Left */}
         <div className='w-full sm:w-1/2 flex items-center justify-center py-10 sm:py-0'>
-          <div className='text-[#414141'>
+          <div className='text-[#414141]'>
             <div className='flex items-center gap-2'>
               <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
               <p className='font-medium text-sm md:text-base'>OUR BESTSELLER</p>
@@ -22,6 +22,7 @@ const Hero = () => {
 
         {/* Hero Right */}
         <img src={assets.hero_img} className='w-full sm:w-1/2' alt="" />
+
       </div>
     </div>
   )
