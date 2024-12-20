d3711b90172915f64d7238eeee60ee9d42dc8280
ugorji eke <ugorjiekechibuzo@gmail.com>
2024-12-20 19:41:21 +0100

    server update

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,14 @@ connectCloundinary();
 
 // Middlewares
 app.use(express.json()); // allows us to parse json
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://nazycollection.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 app.use(cors({
   origin: "https://nazycollection.vercel.app",
   credentials: true,
